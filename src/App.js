b5316b9675728df1a813bd3e01fbf8ece73cb3c7
faisal <mohd.faisal@blubook.co.in>
2024-01-30 17:27:48 +0530

    learning hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const data = "Faisal";
+  const [myName, setMyName] = useState(data);
+  const [count, setCount] = useState(0);
+
+  const changeName = () => {
+    setMyName("Hello Everyone");
+    setCount(count + 1);
+    setValues({ email: "", textarea: "" });
+  };
+ const handleInpurChange=(e)=>{
+
+  const {name,value}=e.target;
+  setValues({...myval,[name]:value});
+ }
+
+  const [myval, setValues] = useState({ email: "", textarea: "" });
+
+ const [click1,click2]=useState('pleaseclick')
+ const ToggleEvent1=()=>{
+  let val=click1
+  if(val==='pleaseclick'){
+    click2('Your Welcome')
+  }
+  else{
+    click2('pleaseclick')
+  }
+ }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Your Entered Values are {myval.email} and {myval.textarea}</p>
+      <h1>{myName}</h1>
+
+      <h3>{click1}</h3>
+      <p>You clicked me {count} times</p>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+        <input type="email" value={myval.email} name='email' onChange={handleInpurChange} className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+        <textarea className="form-control" value={myval.textarea} name='textarea' onChange={handleInpurChange} id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+      <button className='btn button-n' onClick={changeName}>Submit</button>
+      <button className='btn color-brder' onClick={ToggleEvent1}>toggle button</button>
+    </>
   );
 }
 
