d2da713f24e39b92f708334fcef159015676feed
faisal <fmohd743@gmail.com>
2024-02-20 21:48:19 +0530

    homepage modified

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -44,28 +44,44 @@ function Home() {
         </div>
       </div>
 
-
-      <div className='container'>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
-          <div className="card-body">
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
-          <div className="card-body">
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
-          <div className="card-body">
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
+      <div className="container-fluid">
+  <div className="row">
+    <div className="col-md-3">
+      <div className="card">
+        <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
+        <div className="card-body">
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
       </div>
+    </div>
+    <div className="col-md-3">
+      <div className="card">
+        <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
+        <div className="card-body">
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div className="col-md-3">
+      <div className="card">
+        <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
+        <div className="card-body">
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div className="col-md-3">
+      <div className="card">
+        <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
+        <div className="card-body">
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
+      
     </>
   )
 
