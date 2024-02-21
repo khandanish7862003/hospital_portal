b2bb5a658def74b84109afa20b9dac16cbe538c0
faisal <fmohd743@gmail.com>
2024-02-21 23:34:34 +0530

    favicon is added

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -4,87 +4,22 @@ import Footer from '../Footer/footer'
 function Home() {
   return (
     <>
-      <div className='container-fluid mx-0 '>
-        <div id="carouselExampleCaptions" className="carousel slide">
-          <div className="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src={process.env.PUBLIC_URL + '/surgery1.jpg'} alt="Example" />
-              <div className="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img src={process.env.PUBLIC_URL + '/surgery2.jpg'} alt="Example" />
-              <div className="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
-              <div className="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>Some representative placeholder content for the third slide.</p>
-              </div>
-            </div>
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
-      </div>
-
-      <div className="container-fluid">
-  <div className="row">
-    <div className="col-md-3">
-      <div className="card">
-        <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
-        <div className="card-body">
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
+    <div className="row">
+      <div className="col-md-6">
+     <div className="card text-bg-dark">
+     <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
+  <div className="card-img-overlay">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p className="card-text"><small>Last updated 3 mins ago</small></p>
     </div>
-    <div className="col-md-3">
-      <div className="card">
-        <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
-        <div className="card-body">
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
-    <div className="col-md-3">
-      <div className="card">
-        <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
-        <div className="card-body">
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
-    <div className="col-md-3">
-      <div className="card">
-        <img src={process.env.PUBLIC_URL + '/laboratory.jpg'} alt="Example" />
-        <div className="card-body">
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
+    <div className="col-md-6" >
+      
     </div>
   </div>
-
-  
+  </div>
 </div>
-<Footer />
-      
+     <Footer/> 
     </>
   )
 
